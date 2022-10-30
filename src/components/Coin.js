3cad07336ff476c27dc4e8814c628d28875b9b32
Reza Azad <coc.m.reza@gmail.com>
2022-10-30 20:21:01 +0330

    add css to coin page

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -1,13 +1,16 @@
 import React from 'react';
 
+//css
+import styles from './Coin.module.css'
+
 const Coin = ({name , image , symbol , cap , change , price}) => {
     return (
-        <div>
+        <div className={styles.container}>
             <img src={image} alt='coin' />
-            <span>{name}</span>
-            <span>{symbol.toUpperCase()}</span>
-            <span>$ {price.toLocaleString()}</span>
-            <span>{change.toFixed(2)}</span>
+            <span className={styles.name}>{name}</span>
+            <span className={styles.symbol}>{symbol.toUpperCase()}</span>
+            <span className={styles.currentPrice}>$ {price.toLocaleString()}</span>
+            <span className={change > 0 ? styles.greenPrice : styles.redPrice}>{change.toFixed(2)}</span>
             <span>$ {cap.toLocaleString()}</span>
         </div>
     );
